657d8ec4cc1d0f3c57474b579803f5f11af6512e
seatiel ygnacio rosario <seatiel_2010@hotmail.com>
2017-02-13 17:17:11 -0400

    Parcial

--- Parcial1Ap1-SeatielY/Program.cs
+++ Parcial1Ap1-SeatielY/Program.cs
@@ -15,7 +15,7 @@ namespace Parcial1Ap1_SeatielY
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormularioPrincipal());
         }
     }
 }
